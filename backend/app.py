5c73422d66c017b9b07a33adc85a574d23b49868
tambat22 <terinambat@gmail.com>
2024-04-20 12:10:51 -0700

    added connected gemini logic with frontend

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 import os  # Make sure to import the os module
+from gemini import img_to_text
+
 
 app = Flask(__name__)
 CORS(app)  # Enables CORS for all domains on all routes
@@ -9,9 +11,9 @@ CORS(app)  # Enables CORS for all domains on all routes
 app.config['UPLOAD_FOLDER'] = 'uploads/'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB limit
 
-def img_to_text(image_path):
-    # Placeholder function for processing the image
-    return "Detected text from the image"
+# def img_to_text(image_path):
+#     # Placeholder function for processing the image
+#     return "Detected text from the image"
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
